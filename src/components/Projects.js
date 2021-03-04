3dc00352a8c391986b1bb6761b39920c25556bfe
PatrickDohn <dohnpatrick1993@gmail.com>
2021-03-03 20:33:42 -0500

    start new card styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import './projects.css';
-import ProjectOne from './Projects/ProjectOne'
-import ProjectTwo from './Projects/ProjectTwo'
+import ProjectFour from './Projects/ProjectFour'
 import projectData from '../project-data.json'
 
 function Projects() {
-  const projectTwo = projectData.projectTwo
-  const projectOne = projectData.projectOne
+  // const projectTwo = projectData.projectTwo
+  // const projectOne = projectData.projectOne
   return (
     <div className='project-container' id='projects'>
       <h1>Projects</h1>
       <div className='cards__container'>
-            <ProjectOne data={projectOne}/>
-            <ProjectOne data={projectTwo}/>
-
-
+            <ProjectFour />
       </div>
     </div>
   );
--- src/components/Projects/ProjectFour.js
+++ src/components/Projects/ProjectFour.js
@@ -1,38 +1,16 @@
 import React from 'react'
-import './projectCard.css'
+import './temp.css'
 import KettleImg from '../../images/ProjectTwo.png'
 import MediThanks from '../../images/Medi-thanks.png'
 import WebAssetIcon from '@material-ui/icons/WebAsset';
 
 function ProjectFour() {
   return (
-    <div className="card-container">
-    <section className="img-animation-section">
-      <div className="wrapper">
-        <div className="card-animation">
-          <img className="cardImage" alt="project" src={KettleImg} />
-        </div>
+  <div className="card-container">
+    <div className="stuff">
+        <h1>Hello</h1>
+        <div>this is working</div>
       </div>
-    </section>
-    <div className="card-content">
-      <div className="project-title">
-        <p>General Assembly 2020 Hackathon Winner</p>
-        <h2>MediThanks</h2>
-        <p>Virtual thanks you board</p>
-      </div>
-      <div className="project-desciption">
-        <p>A web app for frontline medical workers to receive appreciation for their work Through a digital forum for public 'thank you cards' (People can sign up and post appreciation for specific hospitals or in general so that they can feel seen and understand their impact)</p>
-      </div>
-      <div className="project-card-links">
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@master/devicon.min.css" />
-          <a href="https://github.com/PB-J/frontline-frontend">
-          <i className="devicon-github-plain dev"></i>
-          </a>
-          <a href="https://pb-j.github.io/frontline-frontend/#/">
-          <WebAssetIcon className='web-icon' />
-          </a>
-      </div>
-    </div>
   </div>
 
   )
